4d4c596a5ee97ee067634773f792951b53cb7019
Thane Gill <me@thanegill.com>
2013-06-30 03:00:21 -0700

    provisioning for Python and Vagrant

--- Manifest/vagrant.bootstrap.sh
+++ Manifest/vagrant.bootstrap.sh
@@ -13,21 +13,8 @@ apt-get -yq upgrade
 # Server componets 
 apt-get -yq install apache2
 
-# Database componets
-# apt-get -yq install postgresql libpq-dev build-dep python-psycopg2
-
 # Python
 apt-get -yq install python-pip python-dev
 
-# Set up virtualenv
-pip install -q virtualenv
-
-virtualenv /home/vagrant/.virtualenvs/$PROJECT_NAME
-
-source /home/vagrant/.virtualenvs/$PROJECT_NAME/bin/activate
-
 # Rereirements for app
-/home/vagrant/.virtualenvs/VISD_Badges/bin/pip install -r /vagrant/Manifest/requirements.pip
-
-# Make virtualenvs owned by vagrant user
-chown -R vagrant:vagrant /home/vagrant/.virtualenvs
+pip install -r /vagrant/Manifest/requirements.pip
