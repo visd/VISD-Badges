211ee213629aa6a1895672f3d0eb2e01d4444940
Thane Gill <me@thanegill.com>
2013-06-30 09:16:11 -0700

    Merged from rest-framework-tutorial

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # VISD Badges
 
-VISD Badges is a way for teachers and educators to improve their professional development in technological areas with the help of online tutorials with skill and badged based rewards.
+Users submit entries of their work to show they have completed a challenge. The challenges are grouped into different skillsets. When a user completes enough challenges in a skillset, that user earns a badge. We can see the badges a user has earned on that user's portfolio.
+
+VISD Badges is a way for teachers and educators to improve their professional development in technological areas.
--- VISD_Badges/urls.py
+++ VISD_Badges/urls.py
@@ -1,17 +1,14 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url, include
+from rest_framework import routers
+from VISD_Badges import views
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browseable API.
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'VISD_Badges.views.home', name='home'),
-    # url(r'^VISD_Badges/', include('VISD_Badges.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 )
